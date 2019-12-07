0e7e77f2525823c1d66a92b121632aa587dbe7c0
Veronica Nutting <veronicanutting@college.harvard.edu>
2019-12-07 16:12:15 -0500

    midway commit

---
+++ js/main.js
@@ -0,0 +1,81 @@
+// Initialize full page layout
+// Adapted from https://github.com/alvarotrigo/fullPage.js/#fullpagejs
+
+var myFullpage = new fullpage('#fullpage', {
+    //Navigation
+    menu: '#menu',
+    lockAnchors: false,
+    anchors:['firstPage', 'secondPage'],
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: ['firstSlide', 'secondSlide'],
+    showActiveTooltip: false,
+    slidesNavigation: true,
+    slidesNavPosition: 'bottom',
+
+    //Scrolling
+    css3: true,
+    scrollingSpeed: 700,
+    autoScrolling: true,
+    fitToSection: true,
+    fitToSectionDelay: 1000,
+    scrollBar: false,
+    easing: 'easeInOutCubic',
+    easingcss3: 'ease',
+    loopBottom: false,
+    loopTop: false,
+    loopHorizontal: true,
+    continuousVertical: false,
+    continuousHorizontal: false,
+    scrollHorizontally: false,
+    interlockedSlides: false,
+    dragAndMove: false,
+    offsetSections: false,
+    resetSliders: false,
+    fadingEffect: false,
+    normalScrollElements: '#element1, .element2',
+    scrollOverflow: false,
+    scrollOverflowReset: false,
+    scrollOverflowOptions: null,
+    touchSensitivity: 15,
+    bigSectionsDestination: null,
+
+    //Accessibility
+    keyboardScrolling: true,
+    animateAnchor: true,
+    recordHistory: true,
+
+    //Design
+    controlArrows: true,
+    verticalCentered: true,
+    sectionsColor : ['#ccc', '#fff'],
+    paddingTop: '3em',
+    paddingBottom: '10px',
+    fixedElements: '#header, .footer',
+    responsiveWidth: 0,
+    responsiveHeight: 0,
+    responsiveSlides: false,
+    parallax: false,
+    parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+    cards: false,
+    cardsOptions: {perspective: 100, fadeContent: true, fadeBackground: true},
+
+    //Custom selectors
+    sectionSelector: '.section',
+    slideSelector: '.slide',
+
+    lazyLoading: true,
+
+    //events
+    onLeave: function(origin, destination, direction){},
+    afterLoad: function(origin, destination, direction){},
+    afterRender: function(){},
+    afterResize: function(width, height){},
+    afterReBuild: function(){},
+    afterResponsive: function(isResponsive){},
+    afterSlideLoad: function(section, origin, destination, direction){},
+    onSlideLeave: function(section, origin, destination, direction){}
+});
+
+//methods
+fullpage_api.setAllowScrolling(true);
