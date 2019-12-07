bc5298cbbfef2f3c9b4502470523c8a4859b1261
Jessica Edwards <jessicae637@gmail.com>
2019-12-07 13:23:06 -0500

    more-vis-stuff

--- js/stackarea.js
+++ js/stackarea.js
@@ -1,9 +1,9 @@
 
 // Create SVG drawing area
-var margin = {top: 40, right: 40, bottom: 60, left: 60};
+var margin = {top: 40, right: 60, bottom: 60, left: 60};
 
 var width2 = 700 - margin.left - margin.right,
-	height2 = 500 - margin.top - margin.bottom;
+	height2 = 700 - margin.top - margin.bottom;
 
 var svgArea = d3.select("#stack-chart").append("svg")
 	.attr("width", width2 + margin.left + margin.right)
@@ -21,111 +21,73 @@ svgArea.append("defs").append("clipPath")
 
 var datasetConcentration = [];
 
-var dataCategories = ["Classics", "Comparative Literature",
-	"Comparative Study of Religion", "East Asian Studies", "English",
-	"Folklore and Mythology", "Germanic Languages and Literatures", "History and Literature",
-	"History of Art and Architecture", "Linguistics", "Music",
-	"Near Eastern Languages and Civilizations", "Philosophy", "Romance Languages and Literatures",
-	"Sanskrit and Indian Studies", "Slavic Languages and Literatures", "South Asian Studies",
-	"Visual and Environmental Studies", "Applied Mathematics", "Biomedical Engineering",
-	"Computer Science", "Electrical Engineering", "Engineering Sciences",
-	"Mechanical Engineering", "Astrophysics", "Biochemical Sciences",
-	"Chemical and Physical Biology", "Chemistry", "Chemistry and Physics",
-	"Earth and Planetary Sciences", "Environmental Science and Public Policy", "Human Developmental and Regenerative Biology",
-	"Human Evolutionary Biology", "Integrative Biology", "Mathematics",
-	"Molecular and Cellular Biology", "Neurobiology", "Organismic and Evolutionary Biology",
-	"Physics", "Statistics", "African and African American Studies",
-	"Anthropology", "Economics", "Government",
-	"History", "History and Science", "Psychology",
-	"Social Studies", "Sociology", "Studies of Women, Gender, and Sexuality",
-	"Special Concentration"];
+var dataCategories = ["Art Film and Visual Studies","Classics","Comparative Literature",
+	"English","Folklore and Mythology","Germanic Languages and Literatures",
+	"History and Literature","History of Art and Architecture","Linguistics",
+	"Music","Near Eastern Languages and Civilizations","Philosophy",
+	"Romance Languages and Literatures","Slavic Languages and Literatures",
+	"South Asian Studies","Theater Dance and Media",
+	"Applied Mathematics","Biomedical Engineering","Computer Science",
+	"Electrical Engineering","Engineering Sciences","Environmental Science and Engineering",
+	"Mechanical Engineering","Astronomy","Chemical and Physical Biology",
+	"Chemistry","Chemistry and Physics","Earth and Planetary Sciences",
+	"Environmental Science and Public Policy","Human Developmental and Regenerative Biology",
+	"Human Evolutionary Biology","Integrative Biology","Mathematics","Molecular and Cellular Biology",
+	"Neuroscience","Physics","Statistics","African and African American Studies",
+	"Anthropology","Comparative Study of Religion","East Asian Studies",
+	"Economics","Government","History","History and Science","Psychology",
+	"Social Studies","Sociology","Studies of Women Gender and Sexuality","Special Concentrations"];
+
+// 17 through 23
 
 // var colorScale = d3.scaleOrdinal(d3.schemeReds[30])
 // 	.domain(dataCategories);
 
 // Try this!
-var colors = dataCategories.map(function (d, i) {
-	return d3.interpolateReds(i / dataCategories.length);
-});
+// var colors = dataCategories.map(function (d, i) {
+// 	return d3.interpolateRdYlBu(i / dataCategories.length);
+// });
 
-var colorScale = d3.scaleOrdinal()
-	.domain(dataCategories)
-	.range(colors);
+var colors = ['#fff7fb','#ece7f2','#d0d1e6','#a6bddb','#74a9cf','#3690c0','#0570b0','#034e7b',
+	'#fff7fb','#ece7f2','#d0d1e6','#a6bddb','#74a9cf','#3690c0','#0570b0','#034e7b',
+
+	'#fee5d9','#fcbba1','#fc9272','#fb6a4a', '#ef3b2c','#cb181d','#99000d',
+
+	'#fff7fb','#ece7f2','#d0d1e6','#a6bddb','#74a9cf','#3690c0','#0570b0','#034e7b',
+	'#fff7fb','#ece7f2','#d0d1e6','#a6bddb','#74a9cf','#3690c0','#0570b0','#034e7b',
+	'#fff7fb','#ece7f2','#d0d1e6','#a6bddb','#74a9cf','#3690c0','#0570b0','#034e7b',
+	'#fff7fb','#ece7f2','#d0d1e6'];
+
+// var colorScale = d3.scaleOrdinal()
+// 	.domain(dataCategories)
+// 	.range(colors);
 
 var parseTime = d3.timeParse("%Y");
 
-d3.select("#combo-box").on("change", updateColor);
+// d3.select("#combo-box").on("change", updateColor);
 
-
-d3.csv("data/oldConcentrationData.csv", function(data) {
+d3.csv("data/newConcentrationData.csv", function(data) {
 
 	datasetConcentration = data;
-	//console.log("hello");
-	//console.log(datasetConcentration);
 
 	datasetConcentration.forEach(function(d){
 
 		// Parse data
 		d.Year = parseTime(d.Year);
-		d["Classics"] = +d["Classics"];
-		d["Comparative Literature"] = +d["Comparative Literature"];
-		d["Comparative Study of Religion"] = +d["Comparative Study of Religion"];
-		d["East Asian Studies"] = +d["East Asian Studies"];
-		d["English"] = +d["English"];
-		d["Folklore and Mythology"] = +d["Folklore and Mythology"];
-		d["Germanic Languages and Literatures"] = +d["Germanic Languages and Literatures"];
-		d["History and Literature"] = +d["History and Literature"];
-		d["History of Art and Architecture"] = +d["History of Art and Architecture"];
-		d["Linguistics"] = +d["Linguistics"];
-		d["Music"] = +d["Music"];
-		d["Near Eastern Languages and Civilizations"] = +d["Near Eastern Languages and Civilizations"];
-		d["Philosophy"] = +d["Philosophy"];
-		d["Romance Languages and Literatures"] = +d["Romance Languages and Literatures"];
-		d["Sanskrit and Indian Studies"] = +d["Sanskrit and Indian Studies"];
-		d["Slavic Languages and Literatures"] = +d["Slavic Languages and Literatures"];
-		d["South Asian Studies"] = +d["South Asian Studies"];
-		d["Visual and Environmental Studies"] = +d["Visual and Environmental Studies"];
-		d["Applied Mathematics"] = +d["Applied Mathematics"];
-		d["Biomedical Engineering"] = +d["Biomedical Engineering"];
-		d["Computer Science"] = +d["Computer Science"];
-		d["Electrical Engineering"] = +d["Electrical Engineering"];
-		d["Engineering Sciences"] = +d["Engineering Sciences"];
-		d["Mechanical Engineering"] = +d["Mechanical Engineering"];
-		d["Astrophysics"] = +d["Astrophysics"];
-		d["Biochemical Sciences"] = +d["Biochemical Sciences"];
-		d["Chemical and Physical Biology"] = +d["Chemical and Physical Biology"];
-		d["Chemistry"] = +d["Chemistry"];
-		d["Chemistry and Physics"] = +d["Chemistry and Physics"];
-		d["Earth and Planetary Sciences"] = +d["Earth and Planetary Sciences"];
-		d["Environmental Science and Public Policy"] = +d["Environmental Science and Public Policy"];
-		d["Human Developmental and Regenerative Biology"] = +d["Human Developmental and Regenerative Biology"];
-		d["Human Evolutionary Biology"] = +d["Human Evolutionary Biology"];
-		d["Integrative Biology"] = +d["Integrative Biology"];
-		d["Mathematics"] = +d["Mathematics"];
-		d["Molecular and Cellular Biology"] = +d["Molecular and Cellular Biology"];
-		d["Neurobiology"] = +d["Neurobiology"];
-		d["Organismic and Evolutionary Biology"] = +d["Organismic and Evolutionary Biology"];
-		d["Physics"] = +d["Physics"];
-		d["Statistics"] = +d["Statistics"];
-		d["African and African American Studies"] = +d["African and African American Studies"];
-		d["Anthropology"] = +d["Anthropology"];
-		d["Economics"] = +d["Economics"];
-		d["Government"] = +d["Government"];
-		d["History"] = +d["History"];
-		d["History and Science"] = +d["History and Science"];
-		d["Psychology"] = +d["Psychology"];
-		d["Social Studies"] = +d["Social Studies"];
-		d["Sociology"] = +d["Sociology"];
-		d["Studies of Women, Gender, and Sexuality"] = +d["Studies of Women, Gender, and Sexuality"];
-		d["Special Concentration"] = +d["Special Concentration"];
+
+		Object.keys(d).forEach(function (key) {
+			if (key != "Year") {
+				d[key] = +d[key];
+			}
+		})
 
 	});
-
+	console.log(datasetConcentration);
 	//console.log(datasetConcentration);
 
 	// Scales and axes
 	var x = d3.scaleTime()
-		.range([0, width2])
+		.range([0, width2 - 20])
 		.domain(d3.extent(datasetConcentration, function(d) { return d.Year; }));
 
 	var y = d3.scaleLinear()
@@ -133,7 +95,7 @@ d3.csv("data/oldConcentrationData.csv", function(data) {
 		.domain([0, 2000]);
 
 	var xAxis = d3.axisBottom()
-		.ticks(5)
+		//.ticks(5)
 		.scale(x);
 
 	var yAxis = d3.axisLeft()
@@ -189,38 +151,38 @@ d3.csv("data/oldConcentrationData.csv", function(data) {
 		.attr("class", "area")
 		.merge(categories)
 		.style("fill", function(d,i) {
-			return colorScale(dataCategories[i]);
+			return colors[i];
 		})
 		.style("stroke", "black")
 		.style("stroke-width", 0.25)
 		.attr("d", function(d) {
 			return area(d);
 		})
+		.style("opacity", 1)
 		// Update tooltip text
 		.on("mouseover", function(d,i) {
 			tooltip.text(dataCategories[i]);
+			d3.select(this).style("opacity", 0.3);
 		})
 		.on("mouseout", function(d) {
 			tooltip.text("");
+			d3.select(this).style("opacity", 1);
 		});
-		// .on("click", function(d){var nextColor = this.style.fill == "red" ? "white" : "red";
-		// 	d3.select(this).style("fill", nextColor);
-		// });
 
 	categories.exit().remove();
 
 	svgArea.append("text")
 		.attr("class", "axis-title")
-		.attr("x", width2)
-		.attr("y",  height2 - 5)
+		.attr("x", width2 / 2 + margin.left - 10)
+		.attr("y",  height2 + 40)
 		.style("text-anchor", "end")
 		.text("Year Awarded");
 
 });
 
-
-function updateColor() {
-	selected_data = d3.select("#combo-box").property("value");
-	console.log(selected_data);
-
-}
+//
+// function updateColor() {
+// 	selected_data = d3.select("#combo-box").property("value");
+// 	console.log(selected_data);
+//
+// }
